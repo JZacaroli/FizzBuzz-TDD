59798ff6ea8a2515b2477a4c61c462199f1e9cf6
JZacaroli <32681029+JZacaroli@users.noreply.github.com>
2020-03-31 14:21:08 +0100

    Initial Commit

---
+++ FizzBuzz/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FizzBuzz
+//
+//  Created by admin on 31/03/2020.
+//  Copyright © 2020 JZ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
